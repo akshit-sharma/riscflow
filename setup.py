c2c629b0266666e38aa59bb123ecf9cf699b120b
akshit-sharma <akshit9sharma@gmail.com>
2024-10-12 09:45:07 -0600

    testing with another version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='riscvflow',
-    version="0.0.5",
+    version="0.1.0",
     description='A library for control flow graph analysis of RISC-V assembly',
     author='Akshit Sharma',
     author_email='akshitsharma@mines.edu',
