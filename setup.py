19b7bb1593aae738d9925f8962f6e80e80639604
akshit-sharma <akshit9sharma@gmail.com>
2024-10-12 19:12:07 -0600

    test v7

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 from pathlib import Path
+import toml
 
-version = {}
-with open(Path(__file__).parent / 'riscvflow' / 'version.py') as fp:
-    exec(fp.read(), version)
+# Path to the .bumpversion.toml file
+bumpversion_file = Path(__file__).parent / '.bumpversion.toml'
+
+# Read the .bumpversion.toml file and extract the version
+bumpversion_data = toml.load(bumpversion_file)
+current_version = bumpversion_data['tool']['bumpversion']['current_version']
 
 setup(
     name='riscvflow',
-    version=version['__version__'],
+    version=current_version,
     description='A library for control flow graph analysis of RISC-V assembly',
     author='Akshit Sharma',
     author_email='akshitsharma@mines.edu',
