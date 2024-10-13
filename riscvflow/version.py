6b231d2e99a9dce0c2f5bc1090203040fa2cfd45
akshit-sharma <akshit9sharma@gmail.com>
2024-10-12 19:28:52 -0600

    test v8

--- riscvflow/version.py
+++ riscvflow/version.py
@@ -1,1 +1,14 @@
-__version__ = "0.3.3"
+from pathlib import Path
+import re
+
+current_version_regex = re.compile(r'current_version\s*=\s*\"(\d+\.\d+\.\d+)\"')
+
+with open(Path(__file__).parent / '.bumpversion.toml', 'r') as f:
+    bumpversion_toml = f.read()
+    for line in bumpversion_toml.split('\n'):
+        match = current_version_regex.match(line)
+        if match:
+            current_version = match.group(1)
+            break
+
+__version__ = current_version
--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 from pathlib import Path
-import toml
+import re
 
-# Path to the .bumpversion.toml file
-bumpversion_file = Path(__file__).parent / '.bumpversion.toml'
+current_version_regex = re.compile(r'current_version\s*=\s*\"(\d+\.\d+\.\d+)\"')
 
-# Read the .bumpversion.toml file and extract the version
-bumpversion_data = toml.load(bumpversion_file)
-current_version = bumpversion_data['tool']['bumpversion']['current_version']
+with open(Path(__file__).parent / '.bumpversion.toml', 'r') as f:
+    bumpversion_toml = f.read()
+    for line in bumpversion_toml.split('\n'):
+        match = current_version_regex.match(line)
+        if match:
+            current_version = match.group(1)
+            break
 
 setup(
     name='riscvflow',
